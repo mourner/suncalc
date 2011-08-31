fb5b981b38c30b61bc48ab62c0593801adb534e1
Vladimir Agafonkin <agafonkin@gmail.com>
2011-09-01 01:57:52 +0300

    Fix typo (night <-> nightEnd)

--- suncalc.js
+++ suncalc.js
@@ -83,7 +83,7 @@
 	             [ -0.3, 'sunriseEnd',    'sunsetStart' ],
 	             [   -6, 'dawn',          'dusk'        ],
 	             [  -12, 'nauticalDawn',  'nauticalDusk'],
-	             [  -18, 'night',         'nightEnd'    ],
+	             [  -18, 'nightEnd',      'night'       ],
 	             [    6, 'goldenHourEnd', 'goldenHour'  ]];
 
 	
