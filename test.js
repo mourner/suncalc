c22bcdd8b302bb8d620ed38fd17a8764aef3b143
Vladimir Agafonkin <agafonkin@gmail.com>
2013-03-06 16:16:44 +0200

    add tests for times

--- test.js
+++ test.js
@@ -16,4 +16,19 @@ assert.deepEqual(sc.getPosition(date, lat, lng), {
 	altitude: -0.7000406838781611
 });
 
-console.log(sc.getTimes(date, lat, lng));
+var times = sc.getTimes(date, lat, lng);
+
+assert.equal(times.solarNoon.toUTCString(), new Date("2013-03-05T10:10:57Z").toUTCString());
+assert.equal(times.nadir.toUTCString(), new Date("2013-03-04T22:10:57Z").toUTCString());
+assert.equal(times.sunrise.toUTCString(), new Date("2013-03-05T04:34:57Z").toUTCString());
+assert.equal(times.sunset.toUTCString(), new Date("2013-03-05T15:46:56Z").toUTCString());
+assert.equal(times.sunriseEnd.toUTCString(), new Date("2013-03-05T04:38:19Z").toUTCString());
+assert.equal(times.sunsetStart.toUTCString(), new Date("2013-03-05T15:43:34Z").toUTCString());
+assert.equal(times.dawn.toUTCString(), new Date("2013-03-05T04:02:17Z").toUTCString());
+assert.equal(times.dusk.toUTCString(), new Date("2013-03-05T16:19:36Z").toUTCString());
+assert.equal(times.nauticalDawn.toUTCString(), new Date("2013-03-05T03:24:31Z").toUTCString());
+assert.equal(times.nauticalDusk.toUTCString(), new Date("2013-03-05T16:57:22Z").toUTCString());
+assert.equal(times.nightEnd.toUTCString(), new Date("2013-03-05T02:46:17Z").toUTCString());
+assert.equal(times.night.toUTCString(), new Date("2013-03-05T17:35:36Z").toUTCString());
+assert.equal(times.goldenHourEnd.toUTCString(), new Date("2013-03-05T05:19:01Z").toUTCString());
+assert.equal(times.goldenHour.toUTCString(), new Date("2013-03-05T15:02:52Z").toUTCString());
