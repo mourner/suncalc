270fe2d66d9c085b1635bfe9e65830a16d8a5162
Vladimir Agafonkin <agafonkin@gmail.com>
2013-03-07 18:42:59 +0200

    better tests

--- test.js
+++ test.js
@@ -1,36 +1,47 @@
-var sc = require('./suncalc'),
+
+var SunCalc = require('./suncalc'),
 	assert = require('assert');
 
 var date = new Date('2013-03-05UTC'),
 	lat = 50.5,
 	lng = 30.5;
 
-var moonPos = sc.getMoonPosition(date, lat, lng);
+
+var sunPos = SunCalc.getPosition(date, lat, lng);
+
+assert.equal(sunPos.azimuth, -2.5003175907168385);
+assert.equal(sunPos.altitude, -0.7000406838781611);
+
+
+var times = SunCalc.getTimes(date, lat, lng);
+
+var testTimes = {
+	solarNoon: "2013-03-05T10:10:57Z",
+	nadir: "2013-03-04T22:10:57Z",
+	sunrise: "2013-03-05T04:34:57Z",
+	sunset: "2013-03-05T15:46:56Z",
+	sunriseEnd: "2013-03-05T04:38:19Z",
+	sunsetStart: "2013-03-05T15:43:34Z",
+	dawn: "2013-03-05T04:02:17Z",
+	dusk: "2013-03-05T16:19:36Z",
+	nauticalDawn: "2013-03-05T03:24:31Z",
+	nauticalDusk: "2013-03-05T16:57:22Z",
+	nightEnd: "2013-03-05T02:46:17Z",
+	night: "2013-03-05T17:35:36Z",
+	goldenHourEnd: "2013-03-05T05:19:01Z",
+	goldenHour: "2013-03-05T15:02:52Z"
+};
+
+for (var i in testTimes) {
+	assert.equal(times[i].toUTCString(), new Date(testTimes[i]).toUTCString());
+}
+
+
+var moonPos = SunCalc.getMoonPosition(date, lat, lng);
 
 assert.equal(moonPos.azimuth, -0.9783999522438226);
 assert.equal(moonPos.altitude, 0.006969727754891917);
 assert.equal(moonPos.distance, 364121.37256256194);
 
-var sunPos = sc.getPosition(date, lat, lng);
+assert.equal(SunCalc.getMoonFraction(date, lat, lng), 0.4848068202456373);
 
-assert.equal(sunPos.azimuth, -2.5003175907168385);
-assert.equal(sunPos.altitude, -0.7000406838781611);
-
-var times = sc.getTimes(date, lat, lng);
-
-assert.equal(times.solarNoon.toUTCString(), new Date("2013-03-05T10:10:57Z").toUTCString());
-assert.equal(times.nadir.toUTCString(), new Date("2013-03-04T22:10:57Z").toUTCString());
-assert.equal(times.sunrise.toUTCString(), new Date("2013-03-05T04:34:57Z").toUTCString());
-assert.equal(times.sunset.toUTCString(), new Date("2013-03-05T15:46:56Z").toUTCString());
-assert.equal(times.sunriseEnd.toUTCString(), new Date("2013-03-05T04:38:19Z").toUTCString());
-assert.equal(times.sunsetStart.toUTCString(), new Date("2013-03-05T15:43:34Z").toUTCString());
-assert.equal(times.dawn.toUTCString(), new Date("2013-03-05T04:02:17Z").toUTCString());
-assert.equal(times.dusk.toUTCString(), new Date("2013-03-05T16:19:36Z").toUTCString());
-assert.equal(times.nauticalDawn.toUTCString(), new Date("2013-03-05T03:24:31Z").toUTCString());
-assert.equal(times.nauticalDusk.toUTCString(), new Date("2013-03-05T16:57:22Z").toUTCString());
-assert.equal(times.nightEnd.toUTCString(), new Date("2013-03-05T02:46:17Z").toUTCString());
-assert.equal(times.night.toUTCString(), new Date("2013-03-05T17:35:36Z").toUTCString());
-assert.equal(times.goldenHourEnd.toUTCString(), new Date("2013-03-05T05:19:01Z").toUTCString());
-assert.equal(times.goldenHour.toUTCString(), new Date("2013-03-05T15:02:52Z").toUTCString());
-
-assert.equal(sc.getMoonFraction(date, lat, lng), 0.4848068202456373);
