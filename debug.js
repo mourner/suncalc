c20c8490fc6ec97cae0cf3f66274aa7e8f80c1c2
Vladimir Agafonkin <agafonkin@gmail.com>
2017-01-05 17:44:52 +0200

    various cleanup and comments

--- debug.js
+++ debug.js
@@ -4,7 +4,8 @@ var meeus = require('./meeus');
 
 var RAD = Math.PI / 180;
 
-var date = new Date(); //'2016-01-01UTC'
+var date = new Date('2016-01-01UTC');
+// var date = new Date();
 
 console.log(meeus.getPosition(date, 50.5, 30.5).altitude / RAD);
 console.log(old.getPosition(date, 50.5, 30.5).altitude / RAD);
--- meeus.js
+++ meeus.js
@@ -1,43 +1,60 @@
 
 const RAD = Math.PI / 180;
 
+// Unix time to Julian day
 function julian(date) {
-    return 2440588 + date / 86400000 - 0.5;
+    return date / 86400000 + 2440587.5;
 }
 
+// Julian day to Julian centuries since J2000
 function centuries(jd) {
     return (jd - 2451545) / 36525; // 25.1
 }
 
-function geometricMeanLongitude(t) {
-    return RAD * (280.46646 + 36000.76983 * t + 0.0003032 * t * t); // 25.2
+// Geometric mean longitude of the Sun
+function meanLongitude(t) {
+    return RAD * (280.46646 + t * (36000.76983 + t * 0.0003032)); // 25.2
 }
 
+// Geometric mean anomaly of the Sun
 function meanAnomaly(t) {
-    return RAD * (357.52911 + 35999.05029 * t - 0.0001537 * t * t); // 25.3
+    return RAD * (357.52911 + t * (35999.05029 - t * 0.0001537)); // 25.3
 }
 
+// Equation of center of the Sun
 function equationOfCenter(t) {
     const m = meanAnomaly(t);
-    return RAD * (1.914602 - 0.004817 * t - 0.000014 * t * t) * Math.sin(m) +
+    return RAD * (1.914602 - t * (0.004817 - t * 0.000014)) * Math.sin(m) +
         (0.019993 - 0.000101 * t) * Math.sin(2 * m) +
         0.000289 * Math.sin(3 * m);
 }
 
-function trueGeometricLongitude(t) {
-    const L0 = geometricMeanLongitude(t);
+// True geometric longitude of the Sun
+function trueLongitude(t) {
+    const L0 = meanLongitude(t);
     const C = equationOfCenter(t);
     return L0 + C;
 }
 
-function meanObliquity(t) {
-    return RAD * (84381.448 - 46.815 * t - 0.00059 * t * t + 0.001813 * t * t * t) / 3600;
+// Apparent longitude of the Sun
+function apparentLongitude(t) {
+    return trueLongitude(t) - RAD * (0.00569 + 0.00478 * Math.cos(RAD * (125.04 - 1934.136 * t)));
 }
 
+// Mean obliquity of the ecliptic
+function meanObliquity(t) {
+    return RAD * (84381.448 - t * (46.815 - t * (0.00059 + 0.001813 * t))) / 3600;
+}
+
+// Corrected obliquity of the ecliptic for apparent position of the Sun
+function correctedObliquity(t) {
+    return meanObliquity(t) + RAD * 0.00256 * Math.cos(RAD * (125.04 - 1934.136 * t)); // 25.8
+}
+
+// Apparent position of the Sun in equatorial coordinates
 function apparentEquatorial(t) {
-    const c = Math.cos(RAD * (125.04 - 1934.136 * t));
-    const l = trueGeometricLongitude(t) - RAD * (0.00569 - 0.00478 * c); // apparent longitude
-    const e = meanObliquity(t) + RAD * 0.00256 * c; // 25.8
+    const l = apparentLongitude(t);
+    const e = correctedObliquity(t);
     const sl = Math.sin(l);
     const ra = Math.atan2(Math.cos(e) * sl, Math.cos(l)); // 25.6
     const dec = Math.asin(Math.sin(e) * sl); // 25.7
