f1bf7ca6c069ee5384e393dce50d77adbe7de998
Leo <5376265+leoherzog@users.noreply.github.com>
2023-04-23 14:40:34 -0400

    Add Exports for Serverless Platforms
    Adds support for platforms such as Google Apps Script and Cloudflare Workers

--- suncalc.js
+++ suncalc.js
@@ -312,6 +312,8 @@ SunCalc.getMoonTimes = function (date, lat, lng, inUTC) {
 // export as Node module / AMD module / browser variable
 if (typeof exports === 'object' && typeof module !== 'undefined') module.exports = SunCalc;
 else if (typeof define === 'function' && define.amd) define(SunCalc);
-else window.SunCalc = SunCalc;
+else if (globalThis !== 'undefined') globalThis.SunCalc = SunCalc;
+else if (window !== 'undefined') window.SunCalc = SunCalc;
+else self.SunCalc = SunCalc;
 
 }());
